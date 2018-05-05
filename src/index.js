36e0a87847b6bc4bde11cca1bc3864654b95c0ba
Zell Liew <zellwk@gmail.com>
2018-05-05 15:21:26 +0800

    fix typo
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const createRequestOptions = require('./createRequestOptions')
 const handleResponse = require('./handleResponse')
 
 const zlFetch = (url, options) => {
-  const opts = createRequestOptions({ url, options })
+  const opts = createRequestOptions(Object.assign({ url }, options))
   return fetch(opts.url, opts)
     .then(handleResponse)
 }
