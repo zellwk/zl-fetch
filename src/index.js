efb2f5cf5b289b0c8d9254d354ba2dc61e60d71c
Zell Liew <zellwk@gmail.com>
2018-05-05 14:34:35 +0800

    remove unnecessary default args
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 const createRequestOptions = require('./createRequestOptions')
 const handleResponse = require('./handleResponse')
 
-const zlFetch = (url = undefined, options = undefined) => {
+const zlFetch = (url, options) => {
   const opts = createRequestOptions({ url, options })
-
   return fetch(opts.url, opts)
     .then(handleResponse)
 }
