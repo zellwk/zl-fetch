5cb9b99971b0a5b35855fb887fba1ea933ce2fbe
gangsthub <paulmelero@gmail>
2020-02-22 01:07:14 +0100

    show examples in document

--- manual-test/preflight.js
+++ manual-test/preflight.js
@@ -1,10 +1,30 @@
 import zlFetch from '../dist/index.mjs'
+
+const createListOfStats = results => {
+  const list = document.createDocumentFragment('ul')
+  for (let i = 0; i < results.length; i++) {
+    const stat = results[i];
+    const item = document.createElement('li')
+    item.textContent = `${stat.localized_name}, 📈 ${stat.pro_win} %`
+    list.appendChild(item)
+  }
+  return list
+}
+
 // Should work with Open Dota API. (Doesn't fail pre-flight checks)
 export default zlFetch('https://api.opendota.com/api/heroStats', {
-  headers: {}
+  headers: {},
 })
-  .then(res => {
+  .then(async res => {
     console.log('Success: Preflight', res)
+    // append to document
+    const stats = res.body
+    const container = document.getElementById('stats')
+    const titleH2 = document.createElement('h2')
+    titleH2.textContent = 'Stats'
+    const list = createListOfStats(stats)
+    container.appendChild(titleH2)
+    container.appendChild(list)
   })
   .catch(err => {
     console.log('Failure: Preflight')
