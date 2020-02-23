e033d5cfb66fbe33384c4da45375274163ec6d1f
Zell Liew <zellwk@gmail.com>
2020-02-23 13:13:12 +0800

    Got Rollup to work

--- rollup.config.js
+++ rollup.config.js
@@ -4,40 +4,38 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
+const pkg = require('./package.json')
 require('rimraf').sync('dist')
 
-const plugins = [
-  json(),
-  resolve(),
-  globals(),
-  builtins(),
-  babel({
-    exclude: 'node_modules/**', // only transpile our source code
-    runtimeHelpers: true,
-    babelrc: false,
-    presets: [['@babel/env']],
-    plugins: ['@babel/plugin-transform-runtime']
-  }),
-  commonjs()
-]
-
-const createConfig = ({ input, format, ext = 'js' }) => {
-  return {
-    input: `src/${input}.js`,
-    output: {
-      name: 'zlFetch',
-      file: `dist/${input}.${ext}`,
-      format,
-      sourcemap: true
-    },
-    plugins,
-    inlineDynamicImports: true
-  }
+export default {
+  input: 'src/index.js',
+  output: [{
+    file: pkg.module,
+    format: 'esm',
+    sourcemap: true,
+    plugins: [terser()]
+  }, {
+    file: pkg.browser,
+    name: 'zlFetch',
+    format: 'umd',
+    sourcemap: true,
+    plugins: [terser()]
+  }],
+  plugins: [
+    json(),
+    resolve(),
+    globals(),
+    builtins(),
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+      runtimeHelpers: true,
+      babelrc: false,
+      presets: [['@babel/env']],
+      plugins: ['@babel/plugin-transform-runtime']
+    }),
+    commonjs()
+  ],
+  inlineDynamicImports: true
 }
-
-export default [
-  { input: 'index', format: 'esm', ext: 'mjs' },
-  { input: 'index', format: 'esm' },
-  { input: 'index', format: 'umd' }
-].map(createConfig)
