b0154b64df6f03ab4c79922216c899968ef71fae
Zell Liew <zellwk@gmail.com>
2022-09-19 18:14:25 +0800

    convert arrow fuctions to normal functions

--- src/handleResponse.js
+++ src/handleResponse.js
@@ -1,5 +1,5 @@
 // window.fetch response headers contains entries method.
-const getBrowserFetchHeaders = response => {
+function getBrowserFetchHeaders (response) {
   const headers = {}
   for (const [header, value] of response.headers.entries()) {
     headers[header] = value
@@ -8,7 +8,7 @@ const getBrowserFetchHeaders = response => {
 }
 
 // Node fetch response headers does not contain entries method.
-const getNodeFetchHeaders = response => {
+function getNodeFetchHeaders (response) {
   const headers = {}
   const h = response.headers._headers
   for (const header in h) {
@@ -17,13 +17,13 @@ const getNodeFetchHeaders = response => {
   return headers
 }
 
-const getHeaders = response => {
+function getHeaders (response) {
   return response.headers.entries
     ? getBrowserFetchHeaders(response)
     : getNodeFetchHeaders(response)
 }
 
-const formatOutput = (response, body) => {
+function formatOutput (response, body) {
   const headers = getHeaders(response)
   const returnValue = {
     body,
@@ -58,7 +58,7 @@ async function parseResponse (response, type) {
   return formatOutput(response, body)
 }
 
-export const handleResponse = (response, options) => {
+export function handleResponse (response, options) {
   // Allows user to use a custom response parser.
   // We do this because of two reasons. First, this lets users parse responses that zlFetch doesn't support.
   // Second, we do this because we to avoid cloning the response. Why? Some users want to use the response object, which prompted a `clone` in the first place. Unfortunately, we run into an error regarding cloning in Node when the response is large.  The issue happens because Node has a lower internal buffer size. Trying to response.clone() things in Node may end up hanging the process. That's why we don't use `clone`. If the user wants to, we provide them with the option to use a custom parser instead. (Adding highWaterMark does nothing to resolve the problem).
@@ -68,8 +68,8 @@ export const handleResponse = (response, options) => {
   }
 
   const type = response.headers.get('content-type')
-  
-  if(!type) return formatOutput(response, null)
+
+  if (!type) return formatOutput(response, null)
   if (type.includes('json')) return parseResponse(response, 'json')
   if (type.includes('text')) return parseResponse(response, 'text')
   if (type.includes('image')) return parseResponse(response, 'blob')
@@ -85,7 +85,7 @@ export const handleResponse = (response, options) => {
  * Formats all errors into zlFetch style error
  * @param {Object} error - The error object
  */
-export const handleError = error => {
+export function handleError (error) {
   if (error.message === 'Failed to fetch') {
     /* eslint-disable */
     return Promise.reject({ error })
