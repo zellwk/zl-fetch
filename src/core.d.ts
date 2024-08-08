5b3cbb9c76bdff8f6d358d8d2899a40999cffc0b
Tri Rahmat Gunadi <jujiyangasli@gmail.com>
2024-08-08 15:36:25 +0000

    types

--- src/core.d.ts
+++ src/core.d.ts
@@ -1,22 +1,22 @@
 /**
  * Main Fetch Function
  * @param {string} url - endpoint
- * @param {object} options - zlFetch options
+ * @param {object} [options] - zlFetch options
  * @param {string} options.method - HTTP method
- * @param {object} options.headers - HTTP headers
- * @param {object} options.body - Body content
- * @param {string} options.auth - Authentication information
- * @param {string} options.debug - Logs the request options for debugging
- * @param {string} options.returnError - Returns the error instead of rejecting it
- * @param {string} options.customResponseParser - Use a custome response parser
+ * @param {object} [options.headers] - HTTP headers
+ * @param {object} [options.body] - Body content
+ * @param {string} [options.auth] - Authentication information
+ * @param {string} [options.debug] - Logs the request options for debugging
+ * @param {string} [options.returnError] - Returns the error instead of rejecting it
+ * @param {string} [options.customResponseParser] - Use a custome response parser
  */
-export function coreFetch(url: string, options: {
+export function coreFetch(url: string, options?: {
     method: string;
-    headers: object;
-    body: object;
-    auth: string;
-    debug: string;
-    returnError: string;
-    customResponseParser: string;
+    headers?: object;
+    body?: object;
+    auth?: string;
+    debug?: string;
+    returnError?: string;
+    customResponseParser?: string;
 }): Promise<any>;
 export function createShorthandMethods(fn: any): any;
